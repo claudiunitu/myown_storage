ed9f647d211717a37ad7b7bac301eb58da0b8aa1
Name <you@example.com>
2024-11-20 18:27:24 +0200

    fixed dir to vault script for exfat use

--- current_dir_to_vault.sh
+++ current_dir_to_vault.sh
@@ -1,3 +1,3 @@
 CURRENT_DIR_PATH=$(pwd)
 CURRENT_USER=$(whoami)
-sudo flock -x -w 600 /var/lock/myown_storage_rsync.lock rsync -av --delete --temp-dir="/mnt/myown_storage_A/myown_storage_vault_temp" "$CURRENT_DIR_PATH" "/mnt/myown_storage_A/myown_storage_vault/users/$CURRENT_USER"
+sudo flock -x -w 600 /var/lock/myown_storage_rsync.lock rsync --verbose --times --atimes --open-noatime --recursive --delete --temp-dir="/mnt/myown_storage_A/myown_storage_vault_temp" "$CURRENT_DIR_PATH" "/mnt/myown_storage_A/myown_storage_vault/users/$CURRENT_USER"
